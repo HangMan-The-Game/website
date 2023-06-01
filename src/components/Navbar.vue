d4050254132a6d729d2c5f9624525852ab40f805
runzac <91915526+alerunza@users.noreply.github.com>
2023-06-01 10:00:58 +0200

    toggle sticky top from the navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -3,7 +3,8 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 
 <template>
-  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary scrollspy-navbar" style="background-color: #ffffff">
+  <!-- sticky-top -->
+  <nav class="navbar navbar-expand-lg bg-body-tertiary scrollspy-navbar" style="background-color: #ffffff">
     <div class="container">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
         aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
