48e6453fdbbae9cdf249684fd46a8363cedf1ccf
Alessandro Runza <91915526+alerunza@users.noreply.github.com>
2023-06-04 18:33:49 +0200

    add dropdown menu btn

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,9 +2,20 @@
 import { RouterLink, RouterView } from "vue-router";
 </script>
 
+<script>
+export default {
+  data() {
+    return {
+      activeSection: null,
+    };
+  },
+};
+</script>
+
 <template>
   <!-- sticky-top da aggiungere -->
-  <nav class="navbar sticky-top navbar-expand-lg bg-body-tertiary scrollspy-navbar" style="background-color: #ffffff">
+  <nav v-scrollspy="{ active: activeSection, offset: -100 }" class="navbar sticky-top navbar-expand-lg bg-body-tertiary"
+    style="background-color: #ffffff">
     <div class="container">
       <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo03"
         aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
@@ -17,26 +28,35 @@ import { RouterLink, RouterView } from "vue-router";
       <div class="collapse navbar-collapse" id="navbarTogglerDemo03">
         <ul class="navbar-nav me-auto mb-2 mb-lg-0">
           <li class="nav-item fs-5">
-            <RouterLink to="/" class="nav-link" :class="$route.href == '/' ? 'router-link-active' : 'deactivated'">Home
-            </RouterLink>
-          </li>
-          <li class="nav-item fs-5">
-            <a v-if="$route.name == 'home'" class="nav-link" :class="$route.href == '/#about' ? 'router-link-active' : ''"
-              href="#about">About us</a>
-            <a v-if="$route.name != 'home'" class="nav-link" :class="$route.href == '/#about' ? 'router-link-active' : ''"
-              href="/#about">About us</a>
-          </li>
-          <li class="nav-item fs-5">
-            <a v-if="$route.name == 'home'" class="nav-link"
-              :class="$route.href == '/#gamedesc' ? 'router-link-active' : ''" href="#gamedesc">Game Description</a>
-            <a v-if="$route.name != 'home'" class="nav-link"
-              :class="$route.href == '/#gamedesc' ? 'router-link-active' : ''" href="/#gamedesc">Game Description</a>
-          </li>
-          <li class="nav-item fs-5">
-            <a v-if="$route.name == 'home'" class="nav-link" :class="$route.href == '/#team' ? 'router-link-active' : ''"
-              href="#team">Team</a>
-            <a v-if="$route.name != 'home'" class="nav-link" :class="$route.href == '/#team' ? 'router-link-active' : ''"
-              href="/#team">Team</a>
+            <div class="btn-group">
+              <RouterLink to="/" class="nav-link" :class="$route.href == '/' ? 'router-link-active' : 'deactivated'">Home
+              </RouterLink>
+              <button type="button" class="btn dropdown-toggle dropdown-toggle-split" data-bs-toggle="dropdown"
+                aria-expanded="false">
+                <!-- <span class="sr-only"></span> -->
+              </button>
+              <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+                <li>
+                  <a v-if="$route.name == 'home'" class="dropdown-item"
+                    :class="$route.href == '/#about' ? 'router-link-active' : ''" href="#about">About us</a>
+                  <a v-if="$route.name != 'home'" class="dropdown-item"
+                    :class="$route.href == '/#about' ? 'router-link-active' : ''" href="/#about">About us</a>
+                </li>
+                <li>
+                  <a v-if="$route.name == 'home'" class="dropdown-item"
+                    :class="$route.href == '/#gamedesc' ? 'router-link-active' : ''" href="#gamedesc">Game Description</a>
+                  <a v-if="$route.name != 'home'" class="dropdown-item"
+                    :class="$route.href == '/#gamedesc' ? 'router-link-active' : ''" href="/#gamedesc">Game
+                    Description</a>
+                </li>
+                <li>
+                  <a v-if="$route.name == 'home'" class="dropdown-item"
+                    :class="$route.href == '/#team' ? 'router-link-active' : ''" href="#team">Team</a>
+                  <a v-if="$route.name != 'home'" class="dropdown-item"
+                    :class="$route.href == '/#team' ? 'router-link-active' : ''" href="/#team">Team</a>
+                </li>
+              </ul>
+            </div>
           </li>
           <li class="nav-item fs-5">
             <RouterLink to="/about" class="nav-link">Contact</RouterLink>
@@ -59,6 +79,15 @@ import { RouterLink, RouterView } from "vue-router";
 </template>
 
 <style scoped>
+.btn {
+  border: none;
+}
+
+.btn:hover {
+  color: #b33636;
+  box-shadow: rgba(0, 0, 0, 0.24) 0px 3px 8px;
+}
+
 a.nav-link:hover {
   color: #b33636;
 }
