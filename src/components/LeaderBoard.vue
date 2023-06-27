73055b57ffc7f4ca63f3f607c79e285832231a5a
runzac <91915526+alerunza@users.noreply.github.com>
2023-06-28 00:20:21 +0200

    update ui of leaderboard

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -4,67 +4,67 @@
     <h1 class="text-center my-3 fw-bold">Leaderboard <i class="bi bi-trophy-fill text-danger"></i></h1>
     <h4 class="text-center my-3">This LeaderBoard is synced with the Mobile APP and the Web APP in real time.</h4>
     <h5 class="text-center my-3 fst-italic">reach the top!</h5>
-    <div class="border rounded border-danger border-3 shadow w-25 mx-auto">
-        <table class="table table-hover">
-            <thead>
-                <tr>
-                    <th scope="col">#</th>
-                    <th scope="col">Nickname</th>
-                    <th scope="col">Points</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr>
-                    <th scope="row"><i class="bi bi-1-circle-fill text-warning"></i></th>
-                    <td><country-flag country='it' size='small' /> Mich2400</td>
-                    <td>45</td>
-                </tr>
-                <tr>
-                    <th scope="row"><i class="bi bi-2-circle-fill text-secondary"></i></th>
-                    <td><country-flag country='it' size='small' /> alerunza</td>
-                    <td>39</td>
-                </tr>
-                <tr>
-                    <th scope="row"><i class="bi bi-3-circle-fill text-primary"></i></th>
-                    <td><country-flag country='it' size='small' /> zGandy</td>
-                    <td>24</td>
-                </tr>
-                <tr>
-                    <th scope="row">4</th>
-                    <td><country-flag country='it' size='small' /> D3fAul7</td>
-                    <td>19</td>
-                </tr>
-                <tr>
-                    <th scope="row">5</th>
-                    <td><country-flag country='it' size='small' /> emadol</td>
-                    <td>18</td>
-                </tr>
-                <tr>
-                    <th scope="row">6</th>
-                    <td><country-flag country='it' size='small' /> leone567</td>
-                    <td>15</td>
-                </tr>
-                <tr>
-                    <th scope="row">7</th>
-                    <td><country-flag country='it' size='small' /> Sten_</td>
-                    <td>12</td>
-                </tr>
-                <tr>
-                    <th scope="row">8</th>
-                    <td><country-flag country='it' size='small' /> AO206</td>
-                    <td>9</td>
-                </tr>
-                <tr>
-                    <th scope="row">9</th>
-                    <td><country-flag country='it' size='small' /> GabriXR</td>
-                    <td>8</td>
-                </tr>
-                <tr>
-                    <th scope="row">10</th>
-                    <td><country-flag country='it' size='small' /> Matth</td>
-                    <td>5</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+    <table class="rounded shadow w-25 mx-auto table table-hover">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Nickname</th>
+                <th scope="col">Points</th>
+            </tr>
+        </thead>
+        <tbody class="table-group-divider">
+            <tr>
+                <th scope="row"><i class="bi bi-1-circle-fill text-warning"></i></th>
+                <td><country-flag country='it' size='small' /> Mich2400</td>
+                <td>45</td>
+            </tr>
+            <tr>
+                <th scope="row"><i class="bi bi-2-circle-fill text-secondary"></i></th>
+                <td><country-flag country='it' size='small' /> alerunza</td>
+                <td>39</td>
+            </tr>
+            <tr>
+                <th scope="row"><i class="bi bi-3-circle-fill text-primary"></i></th>
+                <td><country-flag country='it' size='small' /> zGandy</td>
+                <td>24</td>
+            </tr>
+            <tr>
+                <th scope="row">4</th>
+                <td><country-flag country='it' size='small' /> D3fAul7</td>
+                <td>19</td>
+            </tr>
+            <tr>
+                <th scope="row">5</th>
+                <td><country-flag country='it' size='small' /> emadol</td>
+                <td>18</td>
+            </tr>
+            <tr>
+                <th scope="row">6</th>
+                <td><country-flag country='it' size='small' /> leone567</td>
+                <td>15</td>
+            </tr>
+            <tr>
+                <th scope="row">7</th>
+                <td><country-flag country='it' size='small' /> Sten_</td>
+                <td>12</td>
+            </tr>
+            <tr>
+                <th scope="row">8</th>
+                <td><country-flag country='it' size='small' /> AO206</td>
+                <td>9</td>
+            </tr>
+            <tr>
+                <th scope="row">9</th>
+                <td><country-flag country='it' size='small' /> GabriXR</td>
+                <td>8</td>
+            </tr>
+            <tr>
+                <th scope="row">10</th>
+                <td><country-flag country='it' size='small' /> Matth</td>
+                <td>5</td>
+            </tr>
+        </tbody>
+    </table>
 </template>
+
+<style scoped></style>
