11f27b04840003e724266cc9bfa9b0bbe176c4af
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-02 14:23:37 +0200

    add new row to gamedesc and fix width

--- src/components/GameDesc.vue
+++ src/components/GameDesc.vue
@@ -5,19 +5,31 @@
     <div class="container my-5">
       <div class="row">
         <h1 class="fw-bold text-center my-5">{{ $t("nav2") }} <i class="bi bi-controller"></i></h1>
-        <div class="col-lg-6 d-flex align-items-center">
-          <img class="img-thumbnail shadow-lg rounded mb-5" src="../assets/images/photo1.jpeg"
+        <div class="col-lg-4 d-flex align-items-center">
+          <img class="img-thumbnail shadow-lg rounded mb-5" src="../assets/images/photo4.jpeg"
             alt="Game Description Image" />
         </div>
         <div class="col-lg-6">
           <p class="fs-4" v-html="$t('gamedesc1')"></p>
         </div>
+      </div>
+      <div class="row">
+
         <div class="col-lg-6">
           <p class="fs-4 mt-5" v-html="$t('gamedesc2')"></p>
         </div>
-        <div class="col-lg-6 d-flex align-items-center">
+        <div class="col-lg-6">
           <img class="img-thumbnail shadow-lg rounded mt-5" src="../assets/images/photo2.jpeg"
             alt="Game Description Image" />
+        </div>
+      </div>
+      <div class="row">
+        <div class="col-lg-6">
+          <img class="img-thumbnail shadow-lg rounded mt-5 mb-5" src="../assets/images/photo5.jpeg"
+            alt="Game Description Image" />
+        </div>
+        <div class="col-lg-4">
+          <p class="fs-4 mt-5" v-html="$t('gamedesc3')"></p>
         </div>
       </div>
     </div>
