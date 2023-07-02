d510c99c761bde03f0454e28b5103289e88aaa6e
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-02 16:05:07 +0200

    edit Footer (added link to school)

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -3,7 +3,8 @@
 <template>
   <footer class="footer">
     <div class="container text-center">
-      <span class="fw-bold"><i class="bi bi-c-circle"></i> HangMan 2023</span>
+      <span class="fw-bold"><i class="bi bi-c-circle"></i> HangMan 2023 x <a style="color: #FF4D4D" class="fw-bold"
+          href="https://www.rapisardidavinci.edu.it/">ITET Rapisardi - Da Vinci</a></span>
     </div>
   </footer>
 </template>
