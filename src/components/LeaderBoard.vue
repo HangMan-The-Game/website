43d442cde9647eb20658ce4a189a8715fee8b598
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-09 23:31:54 +0200

    add sorteduser, fix leaderboard, add loader

--- src/components/LeaderBoard.vue
+++ src/components/LeaderBoard.vue
@@ -72,68 +72,84 @@
 <style scoped></style> -->
 
 <script setup>
-import { collection, getDocs, onSnapshot } from 'firebase/firestore';
-import { ref, onMounted } from 'vue';
+import Loader from '@/components/Loader.vue';
+import { collection, onSnapshot } from 'firebase/firestore';
+import { ref, onMounted, onUnmounted, computed } from 'vue';
 import { db } from '@/firebase.js';
 
 const users = ref([]);
+const isLoading = ref(true);
 
-onSnapshot(collection(db, 'users'), (snapshot) => {
-    const updatedUsers = [];
-    snapshot.forEach((doc) => {
-        updatedUsers.push({
-            id: doc.id,
-            ...doc.data()
+const loadingDelay = async () => {
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    isLoading.value = false;
+};
+
+onMounted(() => {
+
+    loadingDelay();
+    const unsubscribe = onSnapshot(collection(db, 'users'), (snapshot) => {
+        const updatedUsers = [];
+        snapshot.forEach((doc) => {
+            updatedUsers.push({
+                id: doc.id,
+                ...doc.data()
+            });
         });
+        users.value = updatedUsers;
     });
-    users.value = updatedUsers;
+
+    onUnmounted(unsubscribe);
+});
+
+const sortedUsers = computed(() => {
+    return users.value.sort((a, b) => b.points - a.points);
 });
 </script>
 
 <template>
     <div class="container">
-        <table class="rounded w-25 shadow mx-auto table table-hover">
-            <thead>
-                <tr>
-                    <th class="fw-bold h3 t-lead" scope="col">#</th>
-                    <th class="fw-bold h3 t-lead" scope="col">Nickname</th>
-                    <th class="fw-bold h3 t-lead" scope="col">{{ $t("leaderboard.points") }}</th>
-                </tr>
-            </thead>
-            <tbody class="table-group-divider">
-                <tr v-for="(user, index) in users" :key="user.id" :class="{ 'table-danger': index === 0 }">
-                    <th scope="row">
-                        <span v-if="index === 0">
-                            🏆
-                        </span>
-                        <span v-else>
-                            {{ index + 1 }}
-                        </span>
-                    </th>
-                    <td class="t-lead">
-                        <span v-if="user.points >= 100">
-                            😎 {{ user.name }}
-                        </span>
-                        <span v-else-if="user.points >= 50">
-                            🌟 {{ user.name }}
-                        </span>
-                        <span v-else>
-                            {{ user.name }}
-                        </span>
-                    </td>
-                    <td class="t-lead">
-                        <span v-if="user.points >= 100">
-                            💯 {{ user.points }}
-                        </span>
-                        <span v-else-if="user.points >= 50">
-                            ✨ {{ user.points }}
-                        </span>
-                        <span v-else>
-                            {{ user.points }}
-                        </span>
-                    </td>
-                </tr>
-            </tbody>
-        </table>
+        <h1 class="text-center my-3 fw-bold">{{ $t("leaderboard.title") }} <i class="bi bi-trophy-fill text-danger"></i>
+        </h1>
+        <h4 class="text-center my-3">{{ $t("leaderboard.description") }}</h4>
+        <h5 class="text-center my-3 fst-italic">{{ $t("leaderboard.reachTheTop") }}</h5>
+        <Loader v-if="isLoading" class="mx-auto" />
+        <!--         <div v-if="isLoading" class="text-center my-3">
+            <i class="bi bi-arrow-repeat animate-spin text-primary"></i> Loading...
+        </div> -->
+        <div class="container" v-else>
+            <table class="rounded w-50 shadow mx-auto table table-hover">
+                <thead>
+                    <tr>
+                        <th class="fw-bold h3 t-lead" scope="col">#</th>
+                        <th class="fw-bold h3 t-lead" scope="col">Nickname</th>
+                        <th class="fw-bold h3 t-lead" scope="col">{{ $t("leaderboard.points") }}</th>
+                    </tr>
+                </thead>
+                <tbody class="table-group-divider">
+                    <tr v-for="(user, index) in sortedUsers" :key="user.id">
+                        <th scope="row">
+                            <span v-if="index === 0">🥇</span>
+                            <span v-else-if="index === 1">🥈</span>
+                            <span v-else-if="index === 2">🥉</span>
+                            <span v-else>{{ index + 1 }}</span>
+                        </th>
+                        <td class="t-lead">{{ user.name }}</td>
+                        <td class="t-lead">{{ user.points }}</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
     </div>
 </template>
+
+<style>
+.table-group-divider tr:last-child {
+    border-bottom: none;
+}
+
+.table-group-divider tr td,
+.table-group-divider tr th {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.1);
+}
+</style>
