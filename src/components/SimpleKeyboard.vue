27ad2d789e63fb9815440fe0e72a2030d08b9850
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-10 15:28:53 +0200

    create simple keyboard component

---
+++ src/components/SimpleKeyboard.vue
@@ -0,0 +1,31 @@
+<script setup>
+import Keyboard from 'simple-keyboard'
+import 'simple-keyboard/build/css/index.css'
+import { ref, onMounted } from 'vue';
+
+const emit = defineEmits(["onKeyPress"])
+
+const keyboard = ref(null);
+
+const onKeyPress = (button) => {
+    emit("onKeyPress", button)
+};
+
+onMounted(() => {
+    keyboard.value = new Keyboard("simple-keyboard", {
+        layout: {
+            default: [
+                "q w e r t y u i o p",
+                "a s d f g h j k l",
+                "z x c v b n m",
+            ]
+        },
+        onKeyPress: onKeyPress,
+    });
+});
+
+</script>
+
+<template>
+    <div class="simple-keyboard"></div>
+</template>
