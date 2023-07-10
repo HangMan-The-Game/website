4391bc328f6834df1f40a1a426a5f58a417428b6
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-10 15:29:02 +0200

    create game and imported keyboard and keylistener

---
+++ src/components/Game.vue
@@ -0,0 +1,23 @@
+<script setup>
+import { onMounted } from "vue";
+import SimpleKeyboard from "./SimpleKeyboard.vue"
+
+const handleInput = (key) => {
+    console.log(key)
+}
+
+onMounted(() => {
+    window.addEventListener("keyup", (e) => {
+        e.preventDefault();
+        let key =
+            e.key.toUpperCase() + " ";
+        handleInput(key);
+    })
+})
+
+</script>
+<template>
+    <div class="container w-50 mx-auto my-5">
+        <SimpleKeyboard @onKeyPress="handleInput" />
+    </div>
+</template>
