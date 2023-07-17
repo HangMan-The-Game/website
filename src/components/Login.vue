cf4062d50d84838b5b7573f35a2c52e441aa5fb6
runzac <91915526+alerunza@users.noreply.github.com>
2023-07-17 23:03:24 +0200

    add modal (wip)

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,6 +1,6 @@
 <script setup>
 import { auth } from '@/firebase.js'
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile } from 'firebase/auth'
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword, signOut, onAuthStateChanged, updateProfile, sendPasswordResetEmail } from 'firebase/auth'
 import { ref } from 'vue';
 import router from '../router';
 
@@ -84,6 +84,29 @@ async function signout() {
     })
 }
 
+/* async function forgotPassword(email) {
+    try {
+        await sendPasswordResetEmail(auth, email);
+        editMsg.value = 'Email inviata con successo';
+        closeModal(); // Chiude il modal dopo l'invio dell'email
+    } catch (error) {
+        console.error('Errore durante l\'invio dell\'email di reset password:', error);
+        editMsg.value = 'Errore durante l\'invio dell\'email di reset password';
+    }
+}
+ */
+
+async function forgotPassword() {
+    try {
+        // await sendPasswordResetEmail(auth, email.value);
+        editMsg.value = 'Email inviata con successo';
+        closeModal();
+    } catch (error) {
+        console.error('Errore durante l\'invio dell\'email di reset password:', error);
+        editMsg.value = 'Errore durante l\'invio dell\'email di reset password';
+    }
+}
+
 onAuthStateChanged(auth, currentUser => {
     user.value = currentUser
 })
@@ -115,6 +138,40 @@ onAuthStateChanged(auth, currentUser => {
                             <button type="submit" class="btn btn-danger w-100">{{ mode === 'login' ? 'Login' : 'Register'
                             }}</button>
                             <p class="text-warning text-center fw-bold my-3" v-if="errMsg">{{ errMsg }}</p>
+                            <div>
+
+                                <button type="button" class="btn btn-primary mx-auto d-flex justify-content-center mt-2"
+                                    data-bs-toggle="modal" data-bs-target="#exampleModal" data-bs-whatever="@fat">
+                                    test
+                                </button>
+                                <div class=" modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel"
+                                    aria-hidden="true">
+                                    <div class="modal-dialog">
+                                        <div class="modal-content">
+                                            <div class="modal-header">
+                                                <h1 class="modal-title fs-5" id="exampleModalLabel">Modal title</h1>
+                                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                                    aria-label="Close"></button>
+                                            </div>
+                                            <div class="modal-body">
+                                                <div class="mb-3">
+                                                    <label for="recipient-name" class="col-form-label">Recipient:</label>
+                                                    <input type="email" id="email" v-model="data.email"
+                                                        class="form-control">
+                                                </div>
+                                            </div>
+                                            <div class="modal-footer">
+                                                <button type="button" class="btn btn-secondary"
+                                                    data-bs-dismiss="modal">Close</button>
+                                                <button type="button" class="btn btn-primary">Save changes</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- <a class="text-decoration-none fw-bold text-center d-flex justify-content-center"
+                                v-if="errMsg === 'Password errata'" @click="openModal">Forgot the
+                                Password?</a> -->
                             <p class="text-center mb-0">
                                 {{ mode === 'login' ? 'Not registered yet?' : 'Already registered?' }}
                                 <a class="text-decoration-none fw-bold"
